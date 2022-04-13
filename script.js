33f40fc3b3c2a1c044e9c4f2874c847651e4dc13
Roy Dyck <dyck.ro@gmail.com>
2022-04-12 22:08:34 -0500

    Write inital code
    Added evenListeners to the buttons, and added code to push
    numbers to the display, and gave functionality to the 'clear' button

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+function numberPress(number) {
 
+    if (parseFloat(display.innerText) === 0) {
+        display.innerText = +number.textContent;
+    } else {
+        display.innerText += +number.textContent;
+    }
+}
+
+function operatorPress(operator) {
+    alert(operator.id);
+}
+
+function equalPress() {
+    alert('equal');
+}
+
+function clearPress() {
+    display.innerText = 0;
+}
+
+function buttonPress() {
+    const cList = this.classList;
+    switch (true) {
+        case cList.contains('number'):
+            numberPress(this);
+            break;
+        case cList.contains('operator'):
+            operatorPress(this);
+            break;
+        case cList.contains('equal'):
+            equalPress();
+            break;
+        case cList.contains('clear'):
+            clearPress();
+            break;
+        default:
+            console.log("That wasn't supposed to happen");
+            break;
+    }
+}
+
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', buttonPress));
