ae3257bc27788c27aaf9a346e57fac98b8807399
Roy Dyck <dyck.ro@gmail.com>
2022-04-13 11:16:05 -0500

    Add code for operators and equals

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
+function calculate() {
+    switch (operatorMemory) {
+        case "plus":
+            memory += +display.textContent;
+            break;
+        case "minus":
+            memory -= +display.textContent;
+            break;
+        case "multiply":
+            memory *= +display.textContent;
+            break;
+        case "divide":
+            memory /= +display.textContent;
+            break;
+        default:
+            console.log("That wasn't supposed to happen");
+    }
+}
 function numberPress(number) {
 
-    if (parseFloat(display.innerText) === 0) {
-        display.innerText = +number.textContent;
-    } else {
-        display.innerText += +number.textContent;
+    if (parseFloat(display.textContent) === 0 || nonNumberPressed) {
+        display.textContent = +number.textContent;
+        nonNumberPressed = false;
+    } else if (display.textContent.length < 9) {
+        display.textContent += +number.textContent;
     }
+    
 }
 
 function operatorPress(operator) {
-    alert(operator.id);
+    
+    if (memory === null) {
+        memory = +display.textContent;
+    } else {
+        calculate();
+    }
+    display.textContent = memory;
+    nonNumberPressed = true;
+    operatorMemory = operator.id;
 }
 
 function equalPress() {
-    alert('equal');
+    if (operatorMemory !== null) {
+        calculate();
+        display.textContent = memory;
+        memory = null;
+        operatorMemory = null;
+        nonNumberPressed = true;
+    }
 }
 
 function clearPress() {
     display.innerText = 0;
+    memory = null;
+    operatorMemory = null;
 }
 
 function buttonPress() {
@@ -40,6 +76,9 @@ function buttonPress() {
     }
 }
 
+let memory = null;
+let nonNumberPressed = false;
+let operatorMemory = null;
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => button.addEventListener('click', buttonPress));
